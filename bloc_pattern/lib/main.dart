37f3caa72a93e8e2cd59edee14fbd82123315af7
canisterism <ohtthstc@gmail.com>
2020-07-15 01:55:13 +0900

    :tada: create infinite list view

--- bloc_pattern/lib/main.dart
+++ bloc_pattern/lib/main.dart
@@ -1,8 +1,28 @@
+import 'dart:io';
+
+import 'package:bloc_pattern/bloc/bloc.dart';
+import 'package:bloc_pattern/bloc/post_bloc.dart';
+import 'package:bloc_pattern/bloc/post_events.dart';
+import 'package:bloc_pattern/widget/bottom_loader.dart';
+import 'package:bloc_pattern/widget/post_widget.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
-import 'package:bloc_pattern/screens/flash_number_screen.dart';
-import 'package:bloc_pattern/bloc/flash_number_provider.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:http/http.dart';
+import 'package:bloc/bloc.dart';
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    print(transition);
+    super.onTransition(bloc, transition);
+  }
+}
 
 void main() {
+  EquatableConfig.stringify = true;
+  Bloc.observer = SimpleBlocObserver();
   runApp(MyApp());
 }
 
@@ -15,36 +35,80 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: FlashNumberProvider(
-        child: MyHomePage(title: 'Bloc Example'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('InfiniteListViewScreen'),
+        ),
+        body: BlocProvider(
+          create: (_context) =>
+              PostBloc(httpClient: Client())..add(PostFetchStart()),
+          child: InfiniteListViewScreen(),
+        ),
       ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class InfiniteListViewScreen extends StatefulWidget {
+  InfiniteListViewScreen({Key key}) : super(key: key);
 
-  final String title;
+  @override
+  _InfiniteListViewScreenState createState() => _InfiniteListViewScreenState();
+}
+
+class _InfiniteListViewScreenState extends State<InfiniteListViewScreen> {
+  final _scrollController = ScrollController();
+  final _scrollThreshould = 200;
+  PostBloc _postBloc;
+
+  void _onScroll() {
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.position.pixels;
+    if (maxScroll - currentScroll <= _scrollThreshould) {
+      _postBloc.add(PostFetchStart());
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+    _postBloc = BlocProvider.of<PostBloc>(context);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FlatButton(
-              child: Text('FlashNumber'),
-              onPressed: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (c) => FlashNumberScreen())),
-            )
-          ],
-        ),
-      ),
-    );
+    return BlocBuilder<PostBloc, PostState>(builder: (context, state) {
+      // Use if statement, not switch because `if (state is T)` works as type guard.
+      // I prefer to use switch statement, but type guard doesn't work with it.
+      if (state is PostInitial)
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      else if (state is PostSuccess) {
+        if (state.posts.isEmpty) {
+          return Center(
+            child: Text('no posts'),
+          );
+        } else {
+          return ListView.builder(
+              itemBuilder: (context, index) {
+                return index >= state.posts.length
+                    ? BottomLoader()
+                    : PostWidget(post: state.posts[index]);
+              },
+              itemCount: state.posts.length);
+        }
+      } else {
+        // PostFailed
+        return Center(child: Text('failed to fetch posts'));
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 }
